f38989889131cb338cf4fa7da3579e6f82d0c08c
Vinícius Sena <senavinicius01@gmail.com>
2023-10-01 02:08:09 -0300

    Initialize project using Create React App

---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function AppRouter() {
+    return(
+        <BrowserRouter>
+            <Routes>
+            </Routes>
+        </BrowserRouter>
+    )
+}
