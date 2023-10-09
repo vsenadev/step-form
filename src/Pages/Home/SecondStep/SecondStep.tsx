4d326a96dda02baa981dd3980fd929e9e37defd6
Vinícius Sena <senavinicius01@gmail.com>
2023-10-09 00:51:28 -0300

    update

--- src/Pages/Home/SecondStep/SecondStep.tsx
+++ src/Pages/Home/SecondStep/SecondStep.tsx
@@ -9,13 +9,16 @@ export default function SecondStep(){
                 title="Select your plan"
                 description="You have the option of monthly or yearly billing."
             />
-            <div>
+            <div className={styles.container__cards}>
                 {
                     data.plans.map((element) => {
-                        console.log(element.image)
                         return(
-                            <div>
-                                <img src={element.image} alt="icon"/>
+                            <div className={styles.container__cards_div}>
+                                <img src={element.image} alt="icon" className={styles.container__cards_div_image}/>
+                                <div className={styles.container__cards_informations}>
+                                    <h3 className={styles.container__cards_informations_title}>{element.name}</h3>
+                                    <span className={styles.container__cards_informations_price}>${element.monthly}/mo</span>
+                                </div>
                             </div>
                             )
                     })
