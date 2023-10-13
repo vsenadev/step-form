ded473b5f8f8b33e6fc44655ac57f171ad869360
Vinícius Sena <senavinicius01@gmail.com>
2023-10-13 00:31:55 -0300

    Step fourth implement

--- src/Pages/Home/ThirdStep/ThirdStep.tsx
+++ src/Pages/Home/ThirdStep/ThirdStep.tsx
@@ -1,30 +1,71 @@
 import styles from './ThirdStep.module.sass';
 import TitleAndDescription from "../../../Components/TitleAndDescription/TitleAndDescription";
 import data from '../../../data/addons.json';
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function ThirdStep(){
-    return(
+interface AddonsInterface {
+    "id": number,
+    "name": string,
+    "selected": boolean,
+    "description": string,
+    "monthly": number,
+    "yearly": number
+}
+
+export default function ThirdStep() {
+    const [addons, setAddons] = useState<AddonsInterface[]>([]); // Inicialize addons como um array vazio
+
+    function generateAddonsList() {
+        const addonsObject: AddonsInterface[] = data.addons.map((element, index) => ({
+            id: index,
+            name: element.name,
+            selected: false,
+            description: element.description,
+            monthly: element.monthly,
+            yearly: element.yearly
+        }));
+
+        setAddons(addonsObject);
+    }
+
+    const toggleSelection = (itemId: number) => {
+        setAddons((prevItems) =>
+            prevItems.map((item) =>
+                item.id === itemId ? { ...item, selected: !item.selected } : item
+            )
+        );
+    };
+
+    useEffect(() => {
+        generateAddonsList();
+    }, []);
+    console.log(addons)
+    return (
         <section className={styles.container}>
             <TitleAndDescription
                 title="Pick add-ons"
                 description="Add-ons help enhance your gaming experience."
             />
             <div className={styles.container__options}>
-                {
-                    data.addons.map((element) => {
-                        return(
-                            <div key={element.name} className={styles.container__options_card}>
-                                <input type={"checkbox"} className={styles.container__options_card_input}/>
-                                <div className={styles.container__options_card_div}>
-                                    <h4 className={styles.container__options_card_div_name}>{element.name}</h4>
-                                    <p className={styles.container__options_card_div_description}>{element.description}</p>
-                                </div>
-                                <span className={styles.container__options_card_div_price}>+${element.yearly}/yr</span>
+                {addons.map((element) => {
+                    return (
+                        <div key={element.name} className={element.selected ? `${styles.container__options_card_selected} ${styles.container__options_card}` : styles.container__options_card}>
+                            <input
+                                type="checkbox"
+                                checked={element.selected}
+                                onChange={() => toggleSelection(element.id)}
+                                className={styles.container__options_card_input}
+                            />
+                            <div className={styles.container__options_card_div}>
+                                <h4 className={styles.container__options_card_div_name}>{element.name}</h4>
+                                <p className={styles.container__options_card_div_description}>
+                                    {element.description}
+                                </p>
                             </div>
-                        )
-                    })
-                }
+                            <span className={styles.container__options_card_div_price}>+${element.yearly}/yr</span>
+                        </div>
+                    );
+                })}
             </div>
             <div className={styles.container__buttons}>
                 <div className={styles.container__buttons_div}>
@@ -33,5 +74,5 @@ export default function ThirdStep(){
                 </div>
             </div>
         </section>
-    )
+    );
 }
