8b28136471a2301f0103c8baf3dc54bfe1622a4e
Vinícius Sena <senavinicius01@gmail.com>
2023-11-05 00:46:04 -0300

    second step mobile finished

--- src/Pages/HomeMobile/SecondStepMobile/SecondStepMobile.tsx
+++ src/Pages/HomeMobile/SecondStepMobile/SecondStepMobile.tsx
@@ -55,42 +55,45 @@ const AntSwitch = styled(Switch)(({ theme }) => ({
 }));
 export default function SecondStepMobile(props: SecondStepInterface){
     return(
-        <section className={styles.container}>
-            <TitleAndDescription
-                title="Select your plan"
-                description="You have the option of monthly or yearly billing."
-            />
-            <div className={styles.container__cards_mobile}>
-                {
-                    data.plans.map((element) => {
-                        return(
-                            <div className={props.plan === element.name ? `${styles.container__cards_div} ${styles.container__cards_selected}` : styles.container__cards_div} onClick={() => props.setPlan(element.name)} key={element.name}>
-                                <img src={element.image} alt="icon" className={styles.container__cards_div_image}/>
-                                <div className={styles.container__cards_informations}>
-                                    <h3 className={styles.container__cards_informations_title}>{element.name}</h3>
-                                    <span className={styles.container__cards_informations_price}>{props.type ? `${element.monthly}/mo` : `${element.yearly}/yr`}</span>
-                                    <span className={styles.container__cards_informations_free}>{ props.type ? '': `${element.free} months free`}</span>
+        <>
+            <section className={styles.container_mobile}>
+                <TitleAndDescription
+                    title="Select your plan"
+                    description="You have the option of monthly or yearly billing."
+                />
+                <div className={styles.container_mobile__cards_mobile}>
+                    {
+                        data.plans.map((element) => {
+                            return(
+                                <div className={props.plan === element.name ? `${styles.container_mobile__cards_mobile_div_mobile} ${styles.container_mobile__cards_mobile_selected}` : styles.container_mobile__cards_mobile_div_mobile} onClick={() => props.setPlan(element.name)} key={element.name}>
+                                    <img src={element.image} alt="icon" className={styles.container_mobile__cards_mobile_div_mobile_image_mobile}/>
+                                    <div className={styles.container_mobile__cards_mobile_informations}>
+                                        <h3 className={styles.container_mobile__cards_mobile_informations_title}>{element.name}</h3>
+                                        <span className={styles.container_mobile__cards_mobile_informations_price}>{props.type ? `${element.monthly}/mo` : `${element.yearly}/yr`}</span>
+                                        <span className={styles.container_mobile__cards_mobile_informations_free}>{ props.type ? '': `${element.free} months free`}</span>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className={styles.container__toggle}>
-                <Stack direction="row" spacing={3} alignItems="center">
-                    <span className={props.type ? styles.container__toggle_name : styles.container__toggle_opacity}>Monthly</span>
-                    <AntSwitch checked={!props.type}
-                               onClick={() => props.setType(!props.type)}
-                               inputProps={{ 'aria-label': 'ant design' }}/>
-                    <span className={props.type ? styles.container__toggle_name : styles.container__toggle_opacity}>Yearly</span>
-                </Stack>
-            </div>
-            <div className={styles.container__buttons}>
-                <div className={styles.container__buttons_div}>
-                    <span className={styles.container__buttons_div_back} onClick={() => props.setCurrent(1)}>Go Back</span>
-                    <button className={styles.container__buttons_div_next} onClick={() => props.setCurrent(3)}>Next Step</button>
+                            )
+                        })
+                    }
+                </div>
+                <div className={styles.container_mobile__toggle}>
+                    <Stack direction="row" spacing={3} alignItems="center">
+                        <span className={props.type ? styles.container__toggle_name : styles.container__toggle_opacity}>Monthly</span>
+                        <AntSwitch checked={!props.type}
+                                   onClick={() => props.setType(!props.type)}
+                                   inputProps={{ 'aria-label': 'ant design' }}/>
+                        <span className={props.type ? styles.container__toggle_name : styles.container__toggle_opacity}>Yearly</span>
+                    </Stack>
+                </div>
+            </section>
+            <div className={styles.container_mobile__buttons}>
+                <div className={styles.container_mobile__buttons_div}>
+                    <span className={styles.container_mobile__buttons_div_back} onClick={() => props.setCurrent(1)}>Go Back</span>
+                    <button className={styles.container_mobile__buttons_div_next} onClick={() => props.setCurrent(3)}>Next Step</button>
                 </div>
             </div>
-        </section>
+        </>
+
     )
 }
